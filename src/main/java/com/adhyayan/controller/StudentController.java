8aabb30ab6cdde14bcdb834d124476d10dd14e8a
Ajinkya More <ajinkyamore90@gmail.com>
2025-01-30 17:25:11 +0530

    Initial commit

---
+++ src/main/java/com/adhyayan/controller/StudentController.java
@@ -0,0 +1,27 @@
+package com.adhyayan.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.adhyayan.model.Student;
+import com.adhyayan.servicei.StudentServiceI;
+
+@RestController
+public class StudentController
+{
+	@Autowired
+	StudentServiceI si;
+	
+	@GetMapping({"/","/home","/status"})
+	public String homePage()
+	{
+		return "Home Page";
+	}
+	
+	@GetMapping("/getAllStudents")
+	public Iterable<Student> getAllStudents()
+	{
+		return si.getAllStudents();
+	}
+}
